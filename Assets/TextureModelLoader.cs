0828f4668d73f8b13ab869e224439488e9edbe37
Paweł Janusz <trollmenjp@gmail.com>
2023-11-25 18:17:57 +0100

    nextGan

--- Assets/TextureModelLoader.cs
+++ Assets/TextureModelLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98b087f7c692aa2834442cb80a268cc77d845ef27bead7256cf9b1c7f43c30c9
-size 2723
+oid sha256:f4c52037344ac36aa121da387937679a6703c98f2bbb9eea529ee065a26f5475
+size 2727
--- Assets/WaveTextureSaver.cs
+++ Assets/WaveTextureSaver.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b792107a35c74d0b913d2a7b8cfdb164bfe42ac93a90935e56ef3b60f86713c
-size 2777
+oid sha256:9f1c19620790025b6b48f8814ee10e282546565c6314ea92bbef653b3b2ee8b8
+size 3079
