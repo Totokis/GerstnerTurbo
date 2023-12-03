cfa999710ea628fe74da128eb35efb3590e10654
Paweł Janusz <trollmenjp@gmail.com>
2023-12-03 12:40:38 +0100

    feat(Project): Add play loop option

--- Assets/TextureModelLoader.cs
+++ Assets/TextureModelLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c52037344ac36aa121da387937679a6703c98f2bbb9eea529ee065a26f5475
-size 2727
+oid sha256:a8695dd9334ea4579903a6c8443c882dfd86ef7eca96a3194eca2de7cc69c4cf
+size 3548
