5a94cfe9f6fb27f6e199cd2346e7f08cba7293e4
Paweł Janusz <trollmenjp@gmail.com>
2023-12-10 13:49:20 +0100

    feat(Project): Packages update

--- Assets/TextureModelLoader.cs
+++ Assets/TextureModelLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8695dd9334ea4579903a6c8443c882dfd86ef7eca96a3194eca2de7cc69c4cf
-size 3548
+oid sha256:0f66184200823cad30b76c9cf869adc76cc64297f27c7d65f29cdf1d04e4a160
+size 3516
