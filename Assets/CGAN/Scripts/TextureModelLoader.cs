269adface6ff8976ff012fd3e8be3376d8dadf2c
Totokis <pawelj_janusz@wp.pl>
2023-12-10 18:22:28 +0100

    feat(ShaderGraph): Upgrade to shader graph

---
+++ Assets/CGAN/Scripts/TextureModelLoader.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac11b6037194858eb2dae7558a1ea70ce3c66928cfa9928c6be361b65df4f1be
+size 3515
--- Assets/Scripts/TextureModelLoader.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0f66184200823cad30b76c9cf869adc76cc64297f27c7d65f29cdf1d04e4a160
-size 3516
