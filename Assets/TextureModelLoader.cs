d705333555ca953fc8ec833767b1fe5aa3bbd78c
Paweł Janusz <trollmenjp@gmail.com>
2023-12-28 19:53:00 +0100

    feat(Waves): More waves

--- Assets/TextureModelLoader.cs
+++ Assets/TextureModelLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c52037344ac36aa121da387937679a6703c98f2bbb9eea529ee065a26f5475
-size 2727
+oid sha256:2dd8c3860ca090ec8d328435fea5f4d3d173ee2470d59fd63ca23041d91017a6
+size 3558
--- Assets/WaveTextureSaver.cs
+++ Assets/WaveTextureSaver.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f1c19620790025b6b48f8814ee10e282546565c6314ea92bbef653b3b2ee8b8
-size 3079
+oid sha256:afe70dea35cd37a156e6dcaf785c8d939369cbdea10be2d20d7f6c95b75ff3c4
+size 4437
