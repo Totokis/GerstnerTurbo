ee3844b3d822a997ce66f3247e9d684a002aad8d
Paweł Janusz <trollmenjp@gmail.com>
2023-12-31 13:22:40 +0100

    feat(Waves): Add more detailed wave

--- Assets/WaveTextureSaver.cs
+++ Assets/WaveTextureSaver.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe70dea35cd37a156e6dcaf785c8d939369cbdea10be2d20d7f6c95b75ff3c4
+oid sha256:4ae7b96f0dc1cb5e04155486fd0b7561750d14fa6458720f522924704bfaf0c4
 size 4437
